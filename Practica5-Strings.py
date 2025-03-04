36c247f79f4b9f7330c49c41a85a9b6c9223614e
rolloatomico <torres.job.2024@cbtis-246.edu.mx>
2025-03-04 15:36:56 -0600

    Update Practica5-Strings.py

--- Practica5-Strings.py
+++ Practica5-Strings.py
@@ -2,9 +2,16 @@
 #su nombre y apellido, desplegar el texto en 
 #pantalla invertido con un espacio entre ellos
 
+nombre = input("cual es tu nombre")
+apellido = input("cual es tu apellido")
+print(nombre[::-1] ,apellido[::-1])
 
 
 #Crear una solucion que le pregunte al usuario 
 #su nombre y edad. Entonces escribir un mensaje
 #que le diga el año en el que puede llegar a tener
 #100 años de edad
+
+edad = int(input("cuantos años tienes"))
+nombre = input("como te llamas")
+print("en 100 años tendras" ,edad+100)
